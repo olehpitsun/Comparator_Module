c2de6437403dd320173ee13e77d4e8a9a0cf28a0
Vitaliy Netetskiy <vitaliynetetskiy@gmail.com>
2016-02-08 22:03:50 +0200

    added gitingnore

--- Comparator Module/src/Main.java
+++ Comparator Module/src/Main.java
@@ -6,14 +6,15 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.imgcodecs.Imgcodecs;
 import utils.ImageOperations;
 
+import java.io.File;
 import java.util.List;
 
 /**
- * ��� ��� �����������.
- * ������ �������� �� ������ {@link comparator.Comparator} � {@link comparator.MainComparator}
- * ������ �� ����� ���� ������, � ������ ������� ���.
- * � ����� utils ��� ������ �������� ������ �� ��� ������ ({@link utils.GeometryUtils})
- *
+ * тут все запускається.
+ * Велике прохання не чіпати {@link comparator.Comparator} і {@link comparator.MainComparator}
+ * Бажано не міняти коду взагалі, а просто додайте своє.
+ * В пакеті utils вже можуть існувати методи які ваи потрібні ({@link utils.GeometryUtils})
+ * <p>
  * Created by Vit on 07.02.2016.
  */
 public class Main {
@@ -22,26 +23,26 @@ public class Main {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Open CV version - " + Core.VERSION);
+        
+        //завантажуємо зображення
+        Mat imgExpert = Imgcodecs.imread("Comparator Module/images/1_expert.png");
+        Mat imgWatershed = Imgcodecs.imread("Comparator Module/images/1_threshold.png");
+        Mat imgThreshold = Imgcodecs.imread("Comparator Module/images/1_watershed.png");
 
-        //����������� ����������
-        Mat imgExpert = Imgcodecs.imread("C:\\Projects\\Comparator_Module\\Comparator Module\\images\\1_expert.png");
-        Mat imgWatershed = Imgcodecs.imread("C:\\Projects\\Comparator_Module\\Comparator Module\\images\\1_threshold.png");
-        Mat imgThreshold = Imgcodecs.imread("C:\\Projects\\Comparator_Module\\Comparator Module\\images\\1_watershed.png");
-
-        // ��������� � ��������� �������
+        // знаходимо і проріджуємо контури
         List<MatOfPoint> contoursExpert = ImageOperations.prepareContours(imgExpert);
         List<MatOfPoint> contoursWatershed = ImageOperations.prepareContours(imgWatershed);
         List<MatOfPoint> contoursThreshold = ImageOperations.prepareContours(imgThreshold);
 
         MainComparator mainComparator = new MainComparator();
 
-        // ��� ������� �� �����������
+        // тут додаэмо всі компаратори
         mainComparator.add(new HausdorffComparator());
 
-        // �������� ��������� � �������
-        // ������� ���� 0
+        // виводимо результат в консоль
+        // повинно бути 0
         mainComparator.compare(contoursExpert, contoursExpert);
 
         mainComparator.compare(contoursExpert, contoursThreshold);
